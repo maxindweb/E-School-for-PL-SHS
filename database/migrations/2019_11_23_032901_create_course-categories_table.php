dc3f5d618b8ec77fd67b63245606b79906311217
hamonangan silalahi <yohaneshamonangan@ndamelweb.id>
2019-11-23 11:59:03 +0700

    Bakcend add Database

---
+++ database/migrations/2019_11_23_032901_create_course-categories_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCourseCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('course-categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('category_name');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('course-categories');
+    }
+}
---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>E-Learning</title>
+    <link rel="stylesheet" href="{{asset('./css/app.css')}}">
+</head>
+<body>
+    <div id="app">
+    <root-app></root-app>
+    </div>
+    <script src="{{asset('./js/app.js')}}"></script>
+</body>
+</html>
