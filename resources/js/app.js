ddd09184d989bed011583ed1a23149f96eb9df50
hamonangan silalahi <yohaneshamonangan@ndamelweb.id>
2019-11-25 12:19:57 +0700

    frontend intial commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
-require('./bootstrap');
+window.Vue = require('vue');
+
+import Vuetify from 'vuetify'
+import 'vuetify/lib'
+import axios from 'axios'
+import router from './router'
+
+Vue.use(Vuetify)
+window.axios = axios.create()
+
+import RootApp from './components/RootApp.vue';
+const app = new Vue({
+    el:'#app',
+    router,
+    component: [
+        RootApp
+    ]
+})
+
---
+++ resources/js/components/LandingPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app>
+      <v-content>
+          <v-container>
+              <v-row>
+                  
+              </v-row>
+          </v-container>
+      </v-content>
+  </v-app>
+</template>
